e90a18a14c34f92211ff11a4db8466b1c5d5d7f4
BigWalvi <warverson@outlook.com>
2021-01-10 11:07:51 -0300

    knn

---
+++ Classificação/KNN/knn_credit_data.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jan 10 10:05:18 2021
+
+@author: Walvi
+"""
+import pandas as pd
+
+base = pd.read_csv('credit_data.csv')
+base.loc[base.age < 0, 'age'] = 40.92
+
+previsores = base.iloc[:,1:4].values
+classe = base.iloc[:,4].values
+
+from sklearn.impute import SimpleImputer
+imputer = SimpleImputer()
+imputer = imputer.fit(previsores[:,0:3])
+previsores[:,0:3] = imputer.transform(previsores)
+
+from sklearn.preprocessing import StandardScaler
+scaler = StandardScaler()
+previsores = scaler.fit_transform(previsores)
+
+from sklearn.model_selection import train_test_split
+previsores_treinamento, previsores_teste, classe_treinamento, classe_teste = train_test_split(previsores, classe, test_size=0.25, random_state=0)
+
+from sklearn.neighbors import KNeighborsClassifier
+classificador = KNeighborsClassifier(n_neighbors=5, metric='minkowski', p=2)
+classificador.fit(previsores_treinamento, classe_treinamento)
+previsoes = classificador.predict(previsores_teste)
+
+from sklearn.metrics import confusion_matrix, accuracy_score
+precisao = accuracy_score(classe_teste, previsoes)
+matriz = confusion_matrix(classe_teste, previsoes)
+
+from collections import Counter
+Counter(classe_teste)
