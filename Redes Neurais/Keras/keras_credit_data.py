4f6642df587b25d950944b17fbb704c3167b24c7
BigWalvi <warverson@outlook.com>
2021-01-26 09:39:39 -0300

    rede neural keras

---
+++ Redes Neurais/Keras/keras_credit_data.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jan 26 08:14:05 2021
+
+@author: warve
+"""
+import pandas as pd
+
+base = pd.read_csv('credit_data.csv')
+base.loc[base.age < 0, 'age'] = 40.92
+
+previsores = base.iloc[:,1:4].values
+classe = base.iloc[:,4].values
+
+from sklearn.impute import SimpleImputer
+imputer = SimpleImputer()
+imputer = imputer.fit(previsores[:,0:3])
+previsores[:,0:3] = imputer.transform(previsores)
+
+from sklearn.preprocessing import StandardScaler
+scaler = StandardScaler()
+previsores = scaler.fit_transform(previsores)
+
+from sklearn.model_selection import train_test_split
+previsores_treinamento, previsores_teste, classe_treinamento, classe_teste = train_test_split(previsores, classe, test_size=0.25, random_state=0)
+
+import keras
+from keras.models import Sequential
+from keras.layers import Dense
+classificador = Sequential()
+classificador.add(Dense(units=2, activation='relu', input_dim=3))
+classificador.add(Dense(units=2, activation='relu'))
+classificador.add(Dense(units=1, activation='sigmoid'))
+classificador.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
+
+classificador.fit(previsores_treinamento, classe_treinamento, batch_size=25, epochs=100)
+previsoes = classificador.predict(previsores_teste)
+previsoes = (previsoes > 0.5)
+
+from sklearn.metrics import confusion_matrix, accuracy_score
+precisao = accuracy_score(classe_teste, previsoes)
+matriz = confusion_matrix(classe_teste, previsoes)
